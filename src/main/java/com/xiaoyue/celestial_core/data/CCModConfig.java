59e093b443e6ba082d42b17bad1337702c5ad54b
shenenxiaoyue <3371025687@qq.com>
2026-07-25 20:52:19 +0800

    2.0.0-test6

--- src/main/java/com/xiaoyue/celestial_core/data/CCModConfig.java
+++ src/main/java/com/xiaoyue/celestial_core/data/CCModConfig.java
@@ -32,53 +32,38 @@ public class CCModConfig {
             setCelestial();
             builder.push("materials", "Materials");
             fireEssenceChance = builder
-                    .name("Fire Essence Chance")
                     .defineInRange("fireEssenceChance", 0.08, 0, 1);
             oceanEssenceChance = builder
-                    .name("Ocean Essence Chance")
                     .defineInRange("oceanEssenceChance", 0.08, 0, 1);
             deathEssenceChance = builder
-                    .name("Death Essence Chance")
                     .defineInRange("deathEssenceChance", 0.6, 0, 1);
             deathEssenceMinHealth = builder
-                    .name("Death Essence Min Health")
                     .defineInRange("deathEssenceMinHealth", 100, 0, 1000000);
             wardenScleriteChance = builder
-                    .name("Warden Sclerite Chance")
                     .defineInRange("wardenScleriteChance", 0.5, 0, 1);
             shulkerScrapChance = builder
-                    .name("Shulker Scrap Chance")
                     .defineInRange("shulkerScrapChance", 0.5, 0, 1);
             lightFragmentChance = builder
-                    .name("Light Fragment Chance")
                     .defineInRange("lightFragmentChance", 0.05, 0, 1);
             midnightFragmentChance = builder
-                    .name("Midnight Fragment Chance")
                     .defineInRange("midnightFragmentChance", 0.05, 0, 1);
             sakuraFragmentChance = builder
-                    .name("Sakura Fragment Chance")
                     .defineInRange("sakuraFragmentChance", 0.1, 0, 1);
             virtualGoldNuggetChance = builder
-                    .name("virtual Gold Nugget Chance")
                     .defineInRange("virtualGoldNuggetChance", 0.5, 0, 1);
             pureNetherStarEffectCount = builder
-                    .name("Pure Nether Star Effect Count")
                     .defineInRange("pureNetherStarEffectCount", 12, 0, 100);
             guardianSpikeChance = builder
-                    .name("Guardian Spike Chance")
                     .defineInRange("guardianSpikeChance", 0.2, 0, 1);
             brokenTotemChance = builder
-                    .name("Broken Totem Chance")
                     .defineInRange("brokenTotemChance", 0.5, 0, 1);
             builder.pop();
 
             builder.push("items", "Items");
             sakuraToolRecoveryTime = builder
-                    .name("Sakura Tool Recovery Time")
                     .comment("Time required to restore durability each time")
                     .defineInRange("sakuraToolRecoveryTime", 5, 1, 100);
             virtualGoldToolPerBonus = builder
-                    .name("virtual Gold Tool Per Bonus")
                     .comment("Percentage bonus provided by each enchantment")
                     .defineInRange("virtualGoldToolPerAdd", 0.05, 0.01, 1);
             builder.pop();
